74ce8dde63d286db65ef7a04999b18e3967dba67
Alex Fedoseev <alex.fedoseev@gmail.com>
2015-11-23 17:26:31 +0300

    Add installer

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+rm -rf ~/.dotfiles
+git clone git@github.com:alexfedoseev/dotfiles.git ~/.dotfiles
+
+ln -sfn ~/.dotfiles/.bash/.bash_aliases ~/.bash/.bash_aliases
+ln -sfn ~/.dotfiles/.bash/.bash_exports ~/.bash/.bash_exports
+ln -sfn ~/.dotfiles/.bash/.bash_functions ~/.bash/.bash_functions
+ln -sfn ~/.dotfiles/.bash/.bash_prompt ~/.bash/.bash_prompt
+
+ln -sfn ~/.dotfiles/.bash_profile ~/.bash_profile
+ln -sfn ~/.dotfiles/.bashrc ~/.bashrc
+
+ln -sfn ~/.dotfiles/.editorconfig ~/.editorconfig
+
+ln -sfn ~/.dotfiles/.gemrc ~/.gemrc
+
+ln -sfn ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -sfn ~/.dotfiles/.gitignore ~/.gitignore
+
+ln -sfn ~/.dotfiles/.profile ~/.profile
+
+ln -sfn ~/.dotfiles/.vimrc ~/.vimrc
+ln -sfn ~/.dotfiles/.vim/colors/Tomorrow-Night-Bright.vim ~/.vim/colors/Tomorrow-Night-Bright.vim
