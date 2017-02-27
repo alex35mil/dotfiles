eb90666b6c751b7913c5fd968361011440abaa94
Alex Fedoseev <alex.fedoseev@gmail.com>
2017-02-27 15:07:03 +0800

    Update hyper config and keybindings

--- .hyper.js
+++ .hyper.js
@@ -1,13 +1,28 @@
 module.exports = {
   config: {
     fontSize: 14,
-    fontFamily: '"Source Code Pro", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    fontFamily: '"Source Code Pro for Powerline", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
     padding: '40px 40px 30px',
     colors: {},
     shell: '',
     bell: false,
 
     // plugins options
+    paneNavigation: {
+      debug: false,
+      showIndicators: false,
+      hotkeys: {
+        navigation: {
+          up: 'command+alt+up',
+          down: 'command+alt+down',
+          left: 'command+alt+left',
+          right: 'command+alt+right'
+        },
+        jump_prefix: 'command+alt',
+        permutation_modifier: 'shift',
+      },
+    },
+
     hyperStatusLine: {
       footerTransparent: true,
     },
