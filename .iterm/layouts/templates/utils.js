9cabac48158ccb14fe8e965e1f4509660646e8c1
Alex Fedoseev <alex.fedoseev@gmail.com>
2018-02-13 00:55:45 +0800

    Update prompt

--- .iterm/layouts/templates/utils.js
+++ .iterm/layouts/templates/utils.js
@@ -1,9 +1,5 @@
 function prepareSessions(path, sessions) {
-  const navigate = `cd ${path}`;
-  const clear = 'x';
-
-  sessions.forEach(session => session.write({ text: navigate }));
-  sessions.forEach(session => session.write({ text: clear }));
-
+  sessions.forEach(session => session.write({ text: `cd ${path}` }));
+  sessions.forEach(session => session.write({ text: "clear" }));
   delay(1);
 }
--- npm.sh
+++ npm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-yarn global add \
+npm install --global \
   np \
+  now \
+  pure-prompt \
   https://github.com/reasonml/reason-cli/archive/beta-v-1.13.6-bin-darwin.tar.gz
