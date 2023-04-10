b86cbe0b7b90a4f531afee589902f7b3534da7cf
Alex Fedoseev <alex@fedoseev.mx>
2023-04-10 20:15:57 +0400

    shell: add cargo packages

---
+++ cargo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+cargo install \
+    cargo-expand \
+    cargo-outdated \
+    cargo-sync-readme \
+    cargo-update \
+    cargo-wasi \
+    cargo-watch \
+    sea-orm-cli \
+    sqlx-cli \
+    tree-sitter-cli \
+    zellij
