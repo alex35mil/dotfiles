436689344b452798b08ff618d93bde481e70f046
Alex Fedoseev <alex@fedoseev.mx>
2023-06-08 10:25:44 +0400

    nvim: fix telescope file ignore pattern for git

--- user/cfg/nvim/lua/plugins/telescope.lua
+++ user/cfg/nvim/lua/plugins/telescope.lua
@@ -39,7 +39,7 @@ plugin.setup {
             preview_cutoff = 120,
         },
         file_sorter = require("telescope.sorters").get_fuzzy_file,
-        file_ignore_patterns = { ".git/", "node_modules" },
+        file_ignore_patterns = { "%.git/", "node_modules" },
         generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
         path_display = { "truncate" },
         winblend = 0,
