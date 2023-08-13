073f98159f8e4d0241ca3012f8ac7f5fb9c3e7f4
Alex Fedoseev <alex@fedoseev.mx>
2023-08-13 13:54:31 +0400

    nvim: update plugins

--- user/cfg/nvim/lua/plugins.lua
+++ user/cfg/nvim/lua/plugins.lua
@@ -198,7 +198,7 @@ local plugins = {
     -- file tree
     {
         "nvim-neo-tree/neo-tree.nvim",
-        branch = "v2.x",
+        branch = "v3.x",
         dependencies = {
             "nvim-lua/plenary.nvim",
             "nvim-tree/nvim-web-devicons",
