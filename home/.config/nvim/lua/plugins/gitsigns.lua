1264b9e463491c61e04427817776bef906970879
Alex Fedoseev <alex@fedoseev.mx>
2023-09-21 22:05:22 +0400

    nvim: turn off current line blame

--- home/.config/nvim/lua/plugins/gitsigns.lua
+++ home/.config/nvim/lua/plugins/gitsigns.lua
@@ -22,7 +22,7 @@ function M.setup()
             follow_files = true,
         },
         attach_to_untracked = true,
-        current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+        current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
         current_line_blame_opts = {
             virt_text = true,
             virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
