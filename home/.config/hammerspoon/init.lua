39875b1c0a1d1e01b51d026b7e48131dae34a554
Alex Fedoseev <alex@35mil.me>
2024-05-13 00:55:10 +0400

    hammerspoon+karabiner: improve scrolla bindings

--- home/.config/hammerspoon/init.lua
+++ home/.config/hammerspoon/init.lua
@@ -1,6 +1,8 @@
 -- To set the current location of the config:
 -- defaults write org.hammerspoon.Hammerspoon MJConfigFile "~/.config/hammerspoon/init.lua"
 
+--- Mouse
+
 local function move_pointer(direction)
     local pos = hs.mouse.getAbsolutePosition()
     local screen = hs.screen.mainScreen():fullFrame()
@@ -24,3 +26,26 @@ hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "Left", function() move_pointer("left")
 hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "Right", function() move_pointer("right") end)
 hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "Up", function() move_pointer("up") end)
 hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "Down", function() move_pointer("down") end)
+
+--- Scrolla
+
+local function toggle_scrolla(status)
+    local karabiner = "/Library/Application Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli"
+    local enabled
+
+    if status == "on" then
+        enabled = 1
+    elseif status == "off" then
+        enabled = 0
+    end
+
+    local cmd = string.format("'%s' --set-variables '{\"scrolla\":%d}'", karabiner, enabled)
+
+    hs.execute(cmd)
+end
+
+local scrolla_on_watcher = hs.distributednotifications.new(function() toggle_scrolla("on") end, "ScrollaDidEngage")
+local scrolla_off_watcher = hs.distributednotifications.new(function() toggle_scrolla("off") end, "ScrollaDidDisengage")
+
+scrolla_on_watcher:start()
+scrolla_off_watcher:start()
