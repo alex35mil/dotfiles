c03f10f1407ff03c584dba2e53fad32cafce1fa8
Alex <alex@35mil.me>
2024-10-02 10:10:02 +0400

    nvim: annotate K.map

--- home/.config/nvim/lua/keymaps.lua
+++ home/.config/nvim/lua/keymaps.lua
@@ -15,6 +15,15 @@ NVKarabiner = {
 
 local default_keymap_options = { noremap = true, silent = true }
 
+---@class Keymap
+---@field [1] string Keymap
+---@field [2] string Description
+---@field [3] string | function Action
+---@field mode Mode | Mode[] Mode
+
+---@alias Mode "n"|"v"|"i"|"c"|"s"|"o"|"t"|"x"
+
+---@param mapping Keymap
 function K.map(mapping)
     -- NB!: it is important to remove items in reverse order to avoid shifting
     local cmd = table.remove(mapping, 3)
