2bee78b7dc6c2154b50d17d324da38d6a9f12c84
Alex <alex@35mil.me>
2025-07-21 13:57:23 +0300

    fonts: update patch script

--- script/font/patch.sh
+++ script/font/patch.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-docker run --pull always --rm -v ./original/:/in -v ./patched:/out nerdfonts/patcher --complete
+set -o errexit
+set -o nounset
+set -o pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+find "$SCRIPT_DIR/patched" -type f ! -name '.gitkeep' -delete
+
+docker run --pull always --rm -v "$SCRIPT_DIR/original/":/in -v "$SCRIPT_DIR/patched":/out nerdfonts/patcher --complete
