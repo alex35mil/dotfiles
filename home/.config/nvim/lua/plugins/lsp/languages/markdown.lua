5caaad9ae219b957b7dd5c4b1cfcb91ffa83f6b7
Alex <alex@35mil.me>
2026-01-13 22:53:13 +0300

    nvim: wrap in md files

--- home/.config/nvim/lua/plugins/lsp/languages/markdown.lua
+++ home/.config/nvim/lua/plugins/lsp/languages/markdown.lua
@@ -38,4 +38,14 @@ NVMarkdown = {
     },
 }
 
+function NVMarkdown.autocmds()
+    vim.api.nvim_create_autocmd("FileType", {
+        pattern = "markdown",
+        callback = function()
+            vim.opt_local.wrap = true
+            vim.opt_local.linebreak = true -- wrap at word boundaries
+        end,
+    })
+end
+
 return NVMarkdown
