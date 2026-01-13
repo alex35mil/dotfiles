3d0d2d0403e6bce25a4bfa285184682999f0097e
Alex <alex@35mil.me>
2026-01-13 22:51:26 +0300

    nvim: match more with todo comments

--- home/.config/nvim/lua/plugins/todo-comments.lua
+++ home/.config/nvim/lua/plugins/todo-comments.lua
@@ -2,18 +2,19 @@ NVTodoComments = {
     "folke/todo-comments.nvim",
     opts = {
         keywords = {
-            FIX = {
-                icon = " ", -- icon used for the sign, and in search results
-                color = "error", -- can be a hex color, or a named color (see below)
-                alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
-                -- signs = false, -- configure signs for some keywords individually
-            },
-            TODO = { icon = " ", color = "info", alt = { "TODO!" } },
+            FIXME = { icon = " ", color = "error", alt = { "FIX", "BUG" } },
+            TODO = { icon = " ", color = "info" },
             HACK = { icon = " ", color = "warning" },
             WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
             PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
             NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
             TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+        },
+        highlight = {
+            pattern = [=[.*<((KEYWORDS)[!?]?(\([^)]*\)|\[[^]]*\])?)\s*:]=],
+        },
+        search = {
+            pattern = [=[\b(KEYWORDS)[!?]?(\([^)]*\)|\[[^\]]*\])?:]=],
         },
     },
 }
