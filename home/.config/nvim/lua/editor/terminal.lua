49f5c48cdab026bdadc4b2681835494b20a51b0e
Alex <alex@35mil.me>
2026-01-14 15:47:55 +0300

    nvim: update claudecode keymaps

--- home/.config/nvim/lua/editor/terminal.lua
+++ home/.config/nvim/lua/editor/terminal.lua
@@ -5,6 +5,7 @@ local fn = {}
 function NVTerminal.keymaps()
     K.map({ "<D-v>", "Paste text", fn.paste, mode = "t", expr = true })
     K.map({ NVKeymaps.scroll.up, "Exit terminal mode", "<C-\\><C-n>", mode = "t" }) -- it's very convenient with Claude Code as most of the time I exit t mode to scroll up
+    K.map({ NVKeymaps.scroll_alt.up, "Exit terminal mode", "<C-\\><C-n>", mode = "t" }) -- it's very convenient with Claude Code as most of the time I exit t mode to scroll up
     K.map({ NVKeymaps.scroll_ctx.up, "Lazygit: Scroll up main panel", "<C-\\><C-u>", mode = "t" })
     K.map({ NVKeymaps.scroll_ctx.down, "Lazygit: Scroll down main panel", "<C-\\><C-d>", mode = "t" })
 
