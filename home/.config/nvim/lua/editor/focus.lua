d2af1c23b9a108eb06541ac7432a12cdf2d49fe3
Alex <alex@35mil.me>
2026-01-17 11:46:26 +0300

    nvim: improve focus deactivation

--- home/.config/nvim/lua/editor/focus.lua
+++ home/.config/nvim/lua/editor/focus.lua
@@ -1,5 +1,7 @@
 ---@class FocusTab
 ---@field id TabID
+---@field original_tab TabID
+---@field original_win WinID
 ---@field created_at number
 
 ---@class NVFocus
@@ -12,6 +14,7 @@ end
 
 function NVFocus.autocmds()
     vim.api.nvim_create_autocmd("TabClosed", {
+        desc = "Clear focus tab if closed",
         callback = function()
             if NVFocus.tab then
                 local tabs = vim.api.nvim_list_tabpages()
@@ -29,7 +32,6 @@ function NVFocus.autocmds()
                 end
             end
         end,
-        desc = "Clear focus tab if closed",
     })
 end
 
@@ -57,20 +59,24 @@ function NVFocus.toggle()
 
     log.trace({ "No focus tab found. Creating one." })
 
+    local current_tab = vim.api.nvim_get_current_tabpage()
+    local current_win = vim.api.nvim_get_current_win()
+
     vim.cmd("tabnew")
     NVTabs.set_label({ icon = "󰋱", name = "focus" })
 
     local focus_tab = vim.api.nvim_get_current_tabpage()
-    local current_win = vim.api.nvim_get_current_win()
+    local focus_win = vim.api.nvim_get_current_win()
     local empty_buf = vim.api.nvim_get_current_buf()
 
-    vim.api.nvim_win_set_buf(current_win, current_buf)
-    vim.api.nvim_win_set_cursor(current_win, current_cursor)
+    vim.api.nvim_win_set_buf(focus_win, current_buf)
+    vim.api.nvim_win_set_cursor(focus_win, current_cursor)
     vim.api.nvim_buf_delete(empty_buf, { force = true })
-    vim.schedule(NVNoNeckPain.enable)
 
     NVFocus.tab = {
         id = focus_tab,
+        original_tab = current_tab,
+        original_win = current_win,
         created_at = os.time(),
     }
 end
@@ -110,9 +116,35 @@ end
 function NVFocus.deactivate_active()
     local current_cursor = vim.api.nvim_win_get_cursor(0)
     local current_buf = vim.api.nvim_get_current_buf()
-    local tab_number = vim.api.nvim_tabpage_get_number(NVFocus.tab.id)
-    vim.cmd("tabclose " .. tab_number)
 
+    local original_tab = NVFocus.tab.original_tab
+    local original_win = NVFocus.tab.original_win
+    local focus_tab_id = NVFocus.tab.id
+
+    -- Check if original tab still exists
+    local original_tab_valid = false
+    local tabs = vim.api.nvim_list_tabpages()
+    for _, tab in ipairs(tabs) do
+        if tab == original_tab then
+            original_tab_valid = true
+            break
+        end
+    end
+
+    -- Check if original window still exists
+    local original_win_valid = original_tab_valid and vim.api.nvim_win_is_valid(original_win)
+
+    if original_tab_valid then
+        vim.api.nvim_set_current_tabpage(original_tab)
+        if original_win_valid then
+            vim.api.nvim_set_current_win(original_win)
+        end
+    end
+
+    local focus_tab_number = vim.api.nvim_tabpage_get_number(focus_tab_id)
+    vim.cmd("tabclose " .. focus_tab_number)
+
+    -- Restore buffer and cursor in current window
     local current_win = vim.api.nvim_get_current_win()
     vim.api.nvim_win_set_buf(current_win, current_buf)
     vim.api.nvim_win_set_cursor(current_win, current_cursor)
